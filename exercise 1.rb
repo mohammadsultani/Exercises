03481736422711327a21bda126df263b807f379d
mohammad <mohammadsultani44@gmail.com>
2019-12-01 23:29:18 +0800

    Exercise_4 Greeter 2

--- exercise 1.rb
+++ exercise 1.rb
@@ -1,12 +1,1 @@
-print "What is your first name? "
-first_name = gets.chomp . capitalize
-print "What is your last name? "
-last_name = gets.chomp .capitalize
 
-puts "Hello, #{first_name} #{last_name}.How old are you? "
-age = gets.chomp.to_i
-puts "#{first_name} #{last_name}, you will be:"
-puts "#{age + 10} years in 10 years time. "
-puts "#{age + 20} years in 20 years time. "
-puts "#{age + 30} years in 30 years time. "
-puts "#{age + 10} years in 40 years time. "
---
+++ exercise 4.rb
@@ -0,0 +1,21 @@
+print "What is your first name? "
+first_name = gets.chomp . capitalize
+print "What is your last name? "
+last_name = gets.chomp .capitalize
+
+puts "Hello, #{first_name} #{last_name}.How old are you? "
+age = gets.chomp.to_i
+puts "#{first_name} #{last_name}, you will be:"
+puts "#{age + 10} years in 10 years time. "
+puts "#{age + 20} years in 20 years time. "
+puts "#{age + 30} years in 30 years time. "
+puts "#{age + 40} years in 40 years time. "
+print "Your full name reversed is " 
+print " #{first_name}".reverse 
+puts ".#{last_name}".reverse 
+print "Your full name has " 
+print "#{first_name + last_name} ".length
+puts " charachters."
+print "Your full name in uppercase is " 
+print "#{first_name}".upcase
+puts " #{last_name}.".upcase
