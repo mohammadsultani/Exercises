4325c45869130d2f55f9febef2c6313a91eaf53c
mohammad <mohammadsultani44@gmail.com>
2019-12-01 23:55:38 +0800

    Exercise_1 Greeter

--- exercise 1.rb
+++ exercise 1.rb
@@ -1,1 +1,15 @@
+puts "What is your first name? "
+first_name = gets.chomp.capitalize
+puts "What is your last name? "
+last_name = gets.chomp.capitalize
+print "Hello, #{first_name} "
+puts " #{last_name}! How old are you? "
+age = gets.chomp .to_i
+print "#{first_name} " 
+puts " #{last_name}, you will be:"
+puts "#{age + 10} years in 10 years time "
+puts "#{age + 20} years in 20 years time "
+puts "#{age + 30} years in 30 years time "
+puts "#{age + 40} years in 40 years time "
+
 
