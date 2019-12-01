dd01eb439153e417fba2a4649528d60e1d668c2a
mohammad <mohammadsultani44@gmail.com>
2019-12-02 00:18:43 +0800

    Exercise_3-THe nth Place

--- exercise 3.rb
+++ exercise 3.rb
@@ -1,3 +1,9 @@
-def add(a,b)
-    5+2
-end
+puts "What is your number? "	
+number = gets.chomp.to_i 
+puts "The number in the thousandth place is: #{number / 1000}"
+number = number%1000	
+puts "The number in the hundredth place is: #{number / 100}"	
+number = number%100	
+puts "The number in the tens place is: #{number / 10}"	
+number = number%10	
+puts "The number in the ones place is: #{number / 1}"
