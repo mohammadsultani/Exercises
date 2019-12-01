6c3e9c2a6520111eabafbac25c8f2f6bdf3169c5
Mohammad <mohammadsultani44@gmail.com>
2019-12-01 16:51:22 +0800

    Exercise 1 Greater

--- test.rb
+++ test.rb
@@ -1,3 +1,7 @@
-name = "mohammad"
-print name 
-puts "My name is #{name}"
+puts "Welcome to the Ruby Calculator!"
+print "Can I have the first number? "
+first_number = gets.chomp.to_i
+print "Can I have the second number? "
+second_number = gets.chomp.to_i
+puts "cool! The two numbers add up to #{first_number + second_number} "
+puts "#{first_number} - #{second-number} will result in #{first_number - second_number}."
