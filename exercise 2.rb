6b1d83bf620938f971bf5d61a54b89f9f60c5293
mohammad <mohammadsultani44@gmail.com>
2019-12-02 00:34:02 +0800

    Exercise_2 Calculator

--- exercise 2.rb
+++ exercise 2.rb
@@ -1,7 +1,7 @@
 puts "Welcome to the Ruby Calculator!"
-print "Can I have the first number? "
+puts "Can I have the first number? "
 first_number = gets.chomp.to_i
-print "Can I have the second number? "
+puts "Can I have the second number? "
 second_number = gets.chomp.to_i
 puts "cool! The two numbers add up to #{first_number + second_number} "
 puts "#{first_number} - #{second_number} will result in #{first_number - second_number}. "
