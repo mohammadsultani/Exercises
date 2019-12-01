33c088032f3135ef18d7bc06503e93e2dcca5fac
Mohammad <mohammadsultani44@gmail.com>
2019-12-01 17:11:44 +0800

    Exercise 2 The Calculator

--- test.rb
+++ test.rb
@@ -4,4 +4,6 @@ first_number = gets.chomp.to_i
 print "Can I have the second number? "
 second_number = gets.chomp.to_i
 puts "cool! The two numbers add up to #{first_number + second_number} "
-puts "#{first_number} - #{second-number} will result in #{first_number - second_number}."
+puts "#{first_number} - #{second_number} will result in #{first_number - second_number}. "
+puts " Multiplying #{first_number} and #{second_number} will be #{first_number * second_number}"
+puts "Finally, #{first_number} devided by #{second_number} is #{first_number / second_number} "
