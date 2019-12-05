2226a667f1a31bfe939111a5b26dc88be40b2350
mohammad <mohammadsultani44@gmail.com>
2019-12-05 16:58:50 +0800

    Exercise_7 Simple if

--- exercise 7.rb
+++ exercise 7.rb
@@ -1,9 +1,13 @@
-puts "pick a number  "
-num = gets.chomp.to_i
-if num >= 0 && num <= 50
+
+
+puts "pick a number"
+answer = gets.chomp.to_i
+number = answer
+case number 
+when   0..50
     puts "Your number is between 0 and 50 "
-elsif num >= 50 && num <= 100
-    puts "Your number is between 50 and 100 "
-else num > 100
-    puts "Your number is more than 100 "
+when 51..100
+    puts "Your number is between 51 and 100 "
+else 
+    puts "Your namber is more than 100 "
 end
