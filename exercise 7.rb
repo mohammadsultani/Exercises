0414a8a28e294833b168c6a6db6f8bbcb904dffb
mohammad <mohammadsultani44@gmail.com>
2019-12-05 15:15:37 +0800

    if statement

--- exercise 7.rb
+++ exercise 7.rb
@@ -1,4 +1,9 @@
-def exponent(a,b)
-    return a.pow(b)
+puts "pick a number  "
+num = gets.chomp.to_i
+if num >= 0 && num <= 50
+    puts "Your number is between 0 and 50 "
+elsif num >= 50 && num <= 100
+    puts "Your number is between 50 and 100 "
+else num > 100
+    puts "Your number is more than 100 "
 end
- puts exponent(10,0)
