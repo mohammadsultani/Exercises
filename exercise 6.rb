069248ea271c64d94dabbbe7c6652105566e8afa
mohammad <mohammadsultani44@gmail.com>
2019-12-05 13:22:42 +0800

    Exercise-6 Exponent

--- exercise 6.rb
+++ exercise 6.rb
@@ -1,5 +1,9 @@
-def remainder (a,b)
-    a % b   
+def exponent(num=number,power=power)
+   return num ** power   
 end
-puts remainder(8,4)
- 
+puts "Give me a number!"
+number = gets.chomp.to_i
+puts "Give me a power for the number!"
+power = gets.chomp.to_i
+print "#{number} with the power of #{power} is equal to "
+puts exponent(num=number,power=power)
---
+++ exercise 7.rb
@@ -0,0 +1,4 @@
+def exponent(a,b)
+    return a.pow(b)
+end
+ puts exponent(10,0)
