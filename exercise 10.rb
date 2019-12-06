5383bd3b73ba415e9a6fab959b4d598cc73a6f28
mohammad <mohammadsultani44@gmail.com>
2019-12-06 22:48:59 +0800

    Exercise_10 Time Converter

--- exercise 10.rb
+++ exercise 10.rb
@@ -1,9 +1,15 @@
-puts "try a  number"
-a = gets.chomp
-puts "pick a number"
-b = gets.chomp.to_i
+puts "What is the time now? (in military time)"
+time = gets.chomp
+hour = time[0..1].to_i
+minutes = time[2..3]
 
-if a == "male" && (b >= 20 && b <= 40)
-    puts "how are u "
-else puts " wrong number"
+if hour < 12
+    print "#{time} in military time is #{hour}:#{minutes} AM"
+elsif hour == 24
+    print "#{time} in military time is 00:#{minutes} AM"
+elsif hour == 12 
+    print "#{time} in military time is 12:#{minutes} PM"
+else hour > 12 
+    hour = (hour - 12) 
+    print "#{time} in military time is #{hour}:#{minutes} PM"
 end
--- exercise 9.rb
+++ exercise 9.rb
@@ -1,30 +1,64 @@
-puts "what is your gender"
-gender = gets.chomp
-puts "how old are you?"
-age = gets.chomp.to_i
-
-if (gender == "male" && age <= 19)
-puts "You are such a young boy!"
-
-elsif (gender == "male" && (age >= 20 && age <= 40))
-    puts "How is work, mister?"
-
-elsif (gender == "male" && age >= 41)
-    puts "Wow! I admire your experience, sir!"
-elsif (gender == "female" && age <= 19)
-    puts "You're such a young girl!"
-elsif (gender == "female" && (age >= 20 && age <= 40))
-    puts "How is work, miss?"
-elsif (gender == "female" && age >= 41)
-    puts "Wow! I admire your experience, madam!"
-end 
-if age % 2 == 0
-    puts "Your age is an even number."
-else puts "Your age is an odd number."
+def addition(a = first_number,b = second_number)
+    a + b
 end
-birth_year = (2019 - age)
-print "Your birth year is #{birth_year}, " 
-if ((birth_year % 4 == 0) & (birth_year % 100 != 0)) || (birth_year % 400 == 0)
-  puts "and it is a leap year!"
-else puts "and it is a normal year!"
-end  
+def subtraction(a = first_number,b = second_number)
+    a - b 
+end
+def multiplying(a = first_number,b = second_number)
+    a * b
+end
+def division(a = first_number,b = second_number)
+    a / b   
+end 
+def remainder(a = first_number,b = second_number)
+    a % b
+end   
+puts "Can I have the first number? "
+first_number = gets.chomp.to_i
+puts "Can I have the second number? "
+second_number = gets.chomp.to_i
+puts "What opeartion do you want to perform? (+ - * /)"
+operation = gets.chomp
+
+case operation 
+when "+"   
+    print "Cool! The two numbers add up to " 
+puts addition(a = first_number,b = second_number)
+addition
+when "-"
+    print "#{first_number} - #{second_number} will result in "
+    puts subtraction(a = first_number,b = second_number)
+when "*" 
+print "Multiplying #{first_number} and #{second_number} will be "
+puts multiplying(a = first_number,b = second_number)
+
+when "/"  
+print "#{first_number} devided by #{second_number} is "
+print division(a = first_number,b = second_number)
+end
+
+if opration = "/" && (first_number % second_number != 0 )
+    print " with a balance of "
+puts remainder(a = first_number,b = second_number)
+end
+
+# i couldnt figure out how to merge top to bottom  
+if operation = "+"
+    total = first_number + second_number
+
+elsif total > 999
+    puts "The number in the thousandth place is: #{total / 1000}"
+   
+elsif total > 99
+       total = (total % 1000)  
+   puts "The number in the hundredth place is: #{total / 100}"	
+   
+elsif total > 9
+       total = (total % 100)
+   puts "The number in the tens place is: #{total / 10}"	
+   
+elsif total > 0
+       total = (total % 10) 
+   puts "The number in the ones place is: #{total / 1}"
+end
+   
