6ac0c38b9201925621091c5c7e21e4e453f03790
Mohammad sultani <mohammadsultani44@gmail.com>
2019-12-07 23:51:25 +0800

    Ruby Calculator 3

--- exercise 9.rb
+++ exercise 9.rb
@@ -1,64 +1,54 @@
-def addition(a = first_number,b = second_number)
-    a + b
-end
-def subtraction(a = first_number,b = second_number)
-    a - b 
-end
-def multiplying(a = first_number,b = second_number)
-    a * b
-end
-def division(a = first_number,b = second_number)
-    a / b   
-end 
-def remainder(a = first_number,b = second_number)
-    a % b
-end   
-puts "Can I have the first number? "
+ puts "Can I have the first number? "
 first_number = gets.chomp.to_i
 puts "Can I have the second number? "
 second_number = gets.chomp.to_i
-puts "What opeartion do you want to perform? (+ - * /)"
+puts "What arithmetic opeartion do you want to perform? (+ - * /)"
 operation = gets.chomp
 
 case operation 
-when "+"   
-    print "Cool! The two numbers add up to " 
-puts addition(a = first_number,b = second_number)
-addition
+when "+" 
+    num = first_number + second_number  
+     
+    puts "Cool! The two numbers add up to #{num}" 
+
 when "-"
-    print "#{first_number} - #{second_number} will result in "
-    puts subtraction(a = first_number,b = second_number)
-when "*" 
-print "Multiplying #{first_number} and #{second_number} will be "
-puts multiplying(a = first_number,b = second_number)
+    num = first_number - second_number  
+    puts "#{first_number} - #{second_number} will result in #{num}"
+when "*"
+    num = first_number * second_number   
+    puts "Multiplying #{first_number} and #{second_number} will be #{num}"
 
 when "/"  
-print "#{first_number} devided by #{second_number} is "
-print division(a = first_number,b = second_number)
+    num = first_number / second_number  
+     print "#{first_number} devided by #{second_number} is #{num}"
+     if num != 0
+        print " with a balance of " 
+        puts(first_number % second_number)
+     end
+ 
+else 
+    puts "Invalid operation"
+end 
+if num > 99999
+    puts "The number in the ten thousandth is: #{num / 10000}"
 end
+if num > 999
+    puts "The number in the thousandth place is: #{num / 1000}"
+end    
+    (num = num % 1000)
 
-if opration = "/" && (first_number % second_number != 0 )
-    print " with a balance of "
-puts remainder(a = first_number,b = second_number)
-end
+if num > 99
+   puts "The number in the hundredth place is: #{num / 100}"	
+end   
+   (num = num % 100)
+    
+if num > 9
+puts "The number in the tens place is: #{num / 10}"	
+end     
+(num = num % 10)
+if num < 10
+puts "The number in the ones place is: #{num / 1}"
+end                
+ 
 
-# i couldnt figure out how to merge top to bottom  
-if operation = "+"
-    total = first_number + second_number
-
-elsif total > 999
-    puts "The number in the thousandth place is: #{total / 1000}"
-   
-elsif total > 99
-       total = (total % 1000)  
-   puts "The number in the hundredth place is: #{total / 100}"	
-   
-elsif total > 9
-       total = (total % 100)
-   puts "The number in the tens place is: #{total / 10}"	
-   
-elsif total > 0
-       total = (total % 10) 
-   puts "The number in the ones place is: #{total / 1}"
-end
    
