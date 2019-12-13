5fb231353461f3aaf0885bb6b7284b11b3c477dd
Mohammad sultani <mohammadsultani44@gmail.com>
2019-12-13 14:15:26 +0800

    Delete test.rb

--- test.rb
+++
@@ -1,8 +0,0 @@
-def is_coprime?(a,b)
-    if (a%2==0 && b%2==0) || (a%3==0 && b%3==0) || (a%5==0 && b%5==0) || (a%7==0 && b%7==0)
-        p false 
-    else 
-        p true
-    end
-end 
-is_coprime?(25,12)
