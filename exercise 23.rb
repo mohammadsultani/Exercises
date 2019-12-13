bc140b7f4ece3be226dc7b619c31a6446d6264c6
mohammadsultani <mohammadssultani44@gmail.com>
2019-12-13 10:20:05 +0800

    Dutch National Flag Problem

---
+++ exercise 23.rb
@@ -0,0 +1,9 @@
+def segregate(array)
+b = array.select { |x| x > 0 }
+c = array.select { |x| x < 9 }
+seg_array = []
+seg_array.push(c)
+seg_array.push(b)
+print seg_array.flatten
+end
+segregate([0,9,9,9,9,0,9,0,9])
