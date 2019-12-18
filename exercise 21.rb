4c921885640a61d047a7a07736c30272c2ea09e9
mohammadsultani <mohammadssultani44@gmail.com>
2019-12-19 00:49:32 +0800

    Roman Numerals

--- exercise 21.rb
+++ exercise 21.rb
@@ -1,23 +1,57 @@
 def roman_numeral(input_number)
-    m = input_number/1000
-    input_number= input_number%1000
-    d = input_number/500
-    input_number = input_number%500
-    c = input_number/100
-    input_number = input_number%100
-    l = input_number/50
-    input_number = input_number%50
-    x = input_number/10
-    input_number = input_number%10
-    v = input_number/5
-    input_number = input_number%5
-    i = input_number/1
-    m.times {print "M"}
-    d.times {print "D"}
-    c.times {print "C"}
-    l.times {print "L"}
-    x.times {print "X"}
-    v.times {print "V"}
-    i.times {print "I"}
+if input_number > 4999 
+    puts "Please choose a number below 5000!"
+else m = input_number/1000
+    m.times { print "M" }
+    input_number = input_number%1000
+    if input_number >= 900
+        print "CM"
+        input_number= input_number%100
+    end
+    if input_number >= 500
+        print "D"
+        input_number= input_number%500
+    end
+    if input_number >= 400
+        print "CD"
+        input_number = input_number%100
+    end
+    if (input_number >= 100 && input_number < 400)
+        c = input_number/100
+        c.times { print "C"}
+        input_number = input_number%100
+    end
+    if (input_number >= 90 && input_number < 100)
+        print "XC"
+        input_number = input_number%10
+    end
+    if  input_number >= 50 && input_number < 90
+        print "L"
+        input_number = input_number%50
+    end
+    if (input_number >= 40 && input_number < 50)
+        print "XL"
+        input_number = input_number%10
+    end
+    if (input_number > 10 && input_number < 40)
+     x = input_number/10
+     x.times { print "X" }
+     input_number = input_number%10
+    end
+    if input_number == 9
+        print "IX"
+    end
+    if (input_number >= 5 && input_number < 9)
+        print "V"
+        input_number = input_number%5
+    end
+    if input_number == 4
+        print "IV"
+    end
+    if input_number < 4
+        i = input_number/1
+        i.times {print "I"}
+    end
 end
-roman_numeral(444)
+end
+roman_numeral(4780)
---
+++ exercise 38.rb
@@ -0,0 +1,8 @@
+A) /(K|k|s)/ i
+B) /(c[a|u|o]t|b[o|u|a]t)/ i
+C) /([0-9]|[a-j])/ i 
+D) /([^0-9|x|X|\s|\.])/ i
+E) /([^a-z])/ i
+F)  Abc aBc abC
+G)  /abc/ i and /[Aa][Bb][Cc]/ are equivalent regex.
+H)  /(\[\^\w-\w\])/ 
